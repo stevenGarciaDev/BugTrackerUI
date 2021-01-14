d8b7a2d85a06b028c2d0c527c341eacd0ea298f2
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-14 12:26:38 -0800

    Update projects component

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ProjectsTable from '../../components/projects-table';
 
 const Projects = () => (
   <div>
-    Projects
+    <h2>View Projects</h2>
+    <ProjectsTable />
   </div>
 );
 
