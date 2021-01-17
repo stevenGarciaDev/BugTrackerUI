42763fc1725afbbc0859aa03b4ae24fe38a086c7
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-16 19:21:02 -0800

    Update login component
    Add styles for login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {
+  Button,
+} from '../create-project/create-project.styles';
+import {
+  Container,
+  Form,
+  Title,
+  InputControl,
+  Label,
+  Input,
+  ButtonContainer,
+} from './login.styles';
 
 const Login = () => (
-  <div>
-    Login
-  </div>
+  <Container>
+    <Form>
+      <Title>Welcome back</Title>
+      <InputControl>
+        <Label>Email</Label>
+        <Input type="text" />
+      </InputControl>
+      <InputControl>
+        <Label>Password</Label>
+        <Input type="password" />
+      </InputControl>
+      <ButtonContainer>
+        <Button>Login</Button>
+      </ButtonContainer>
+    </Form>
+  </Container>
 );
 
 export default Login;
---
+++ src/pages/login/login.styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: calc(100% - 50px);
+  justify-content: center;
+  position: absolute;
+  top: 60px;
+  width: 100%;
+`;
+
+export const Form = styled.form`
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 20px;
+`;
+
+export const Title = styled.h2`
+  text-align: center;
+`;
+
+export const InputControl = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+`;
+
+export const Label = styled.label`
+
+`;
+
+export const Input = styled.input`
+  border: 1px solid gray;
+  border-radius: 5px;
+  height: 35px;
+  outline: none;
+  margin-top: 5px;
+  padding: 5px;
+  width: 230px;
+
+  @media screen and (min-width: 400px) {
+    width: 350px;
+  }
+
+  @media screen and (min-width: 500px) {
+    width: 400px;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+`;
