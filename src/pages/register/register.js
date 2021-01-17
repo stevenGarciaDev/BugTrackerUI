30908f5d552e92684c5e760b16269bf9e85b0556
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-16 19:22:59 -0800

    Update register comonent

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import {
+  Button,
+} from '../create-project/create-project.styles';
+import {
+  Container,
+  Form,
+  Title,
+  InputControl,
+  Label,
+  Input,
+  ButtonContainer,
+} from '../login/login.styles';
 
 const Register = () => (
-  <div>
-    Register
-  </div>
+  <Container>
+    <Form>
+      <Title>Create an account</Title>
+      <InputControl>
+        <Label>Username</Label>
+        <Input type="text" />
+      </InputControl>
+      <InputControl>
+        <Label>Email</Label>
+        <Input type="text" />
+      </InputControl>
+      <InputControl>
+        <Label>Password</Label>
+        <Input type="password" />
+      </InputControl>
+      <ButtonContainer>
+        <Button>Register</Button>
+      </ButtonContainer>
+    </Form>
+  </Container>
 );
 
 export default Register;
