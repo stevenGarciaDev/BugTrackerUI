b5cc5d880be25e2a0feb0867f3373c80e836dfe3
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-16 17:40:46 -0800

    Update create ticket component

--- src/pages/create-ticket/create-ticket.js
+++ src/pages/create-ticket/create-ticket.js
@@ -6,12 +6,42 @@ import {
 import { Select } from './create-ticket.styles';
 
 const CreateTicket = () => {
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
+  const [ticketForm, setTicketForm] = useState({
+    name: '',
+    description: '',
+    projectTitle: '',
+    ticketType: '',
+    ticketPriority: '',
+    ticketStatus: '',
+    assignedDeveloper: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setTicketForm({
+      ...ticketForm,
+      [name]: value,
+    });
+  };
+
+  const {
+    name,
+    description,
+    projectTitle,
+    ticketType,
+    ticketPriority,
+    ticketStatus,
+    assignedDeveloper,
+  } = ticketForm;
   return (
     <div>
       <h2>Create a New Ticket</h2>
-      <Form>
+      <Form onSubmit={(e) => handleSubmit(e)}>
         <InputContainer>
           <label htmlFor="name">Title</label>
           <Input
@@ -19,26 +49,26 @@ const CreateTicket = () => {
             id="name"
             name="name"
             value={name}
-            onChange={(e) => setName(e.target.value)}
+            onChange={(e) => handleChange(e)}
           />
         </InputContainer>
 
         <InputContainer>
           <label htmlFor="description">Description</label>
-          <Textarea type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)} />
+          <Textarea type="text" id="description" name="description" value={description} onChange={(e) => handleChange(e)} />
         </InputContainer>
 
         <InputContainer>
-          <label htmlFor="description">Project</label>
-          <Select type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)}>
+          <label htmlFor="projectTitle">Project</label>
+          <Select type="text" id="projectTitle" name="projectTitle" value={projectTitle} onChange={(e) => handleChange(e)}>
             <option value="Project 1">1</option>
             <option value="Project 1">2</option>
           </Select>
         </InputContainer>
 
         <InputContainer>
-          <label htmlFor="description">Ticket Type</label>
-          <Select type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)}>
+          <label htmlFor="ticketType">Ticket Type</label>
+          <Select type="text" id="ticketType" name="ticketType" value={ticketType} onChange={(e) => handleChange(e)}>
             <option value="UI">UI</option>
             <option value="Backend">Backend</option>
             <option value="Devops">Devops</option>
@@ -46,8 +76,8 @@ const CreateTicket = () => {
         </InputContainer>
 
         <InputContainer>
-          <label htmlFor="description">Ticket Priority</label>
-          <Select type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)}>
+          <label htmlFor="ticketPriority">Ticket Priority</label>
+          <Select type="text" id="ticketPriority" name="ticketPriority" value={ticketPriority} onChange={(e) => handleChange(e)}>
             <option value="Pending">Pending</option>
             <option value="Low">Low</option>
             <option value="Moderate">Moderate</option>
@@ -58,7 +88,7 @@ const CreateTicket = () => {
 
         <InputContainer>
           <label htmlFor="description">Ticket Status</label>
-          <Select type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)}>
+          <Select type="text" id="description" name="ticketStatus" value={ticketStatus} onChange={(e) => handleChange(e)}>
             <option value="New">New</option>
             <option value="Reviewed">Reviewed</option>
             <option value="In Progress">In Progress</option>
@@ -67,8 +97,8 @@ const CreateTicket = () => {
         </InputContainer>
 
         <InputContainer>
-          <label htmlFor="description">Assigned Developer</label>
-          <Select type="text" id="description" name="description" value={description} onChange={(e) => setDescription(e.target.value)}>
+          <label htmlFor="assignedDeveloper">Assigned Developer</label>
+          <Select type="text" id="assignedDeveloper" name="assignedDeveloper" value={assignedDeveloper} onChange={(e) => handleChange(e)}>
             <option value="Project 1">Pam</option>
             <option value="Project 1">Jim</option>
           </Select>
