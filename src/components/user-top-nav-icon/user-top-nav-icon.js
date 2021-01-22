5160e259c5b8aa32ee2940fb5eae7a217ff43567
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-21 21:31:05 -0800

    Update user top nav icon to use dropdown

--- src/components/user-top-nav-icon/user-top-nav-icon.js
+++ src/components/user-top-nav-icon/user-top-nav-icon.js
@@ -1,16 +1,38 @@
+/* eslint-disable no-console */
 import React, { useState } from 'react';
-import UserDropdownItem from '../user-dropdown-item';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import UserDropdown from '../user-dropdown';
 import { Container, ImageIcon } from './user-top-nav-icon.styles';
+import { selectUserName } from '../../store/user/user.selector';
 
-const UserTopNavIcon = () => {
+const UserTopNavIcon = ({ userName }) => {
   const [isDisplayingDropdown, setDropdownStatus] = useState(false);
+
+  const determineUsernameToDisplay = (name) => {
+    const maxCharactersToDisplay = 18;
+    let nameToDisplay = name;
+    if (nameToDisplay && nameToDisplay.length > maxCharactersToDisplay) {
+      nameToDisplay = `${name.slice(0, maxCharactersToDisplay - 3)}...`;
+    }
+    return nameToDisplay;
+  };
+
   return (
     <Container onClick={() => setDropdownStatus(!isDisplayingDropdown)}>
       <ImageIcon />
-      UserTopNavIcon
-      { isDisplayingDropdown && <UserDropdownItem /> }
+      {determineUsernameToDisplay(userName)}
+      { isDisplayingDropdown && <UserDropdown /> }
     </Container>
   );
 };
 
-export default UserTopNavIcon;
+const mapStateToProps = (state) => ({
+  userName: selectUserName(state),
+});
+
+UserTopNavIcon.propTypes = {
+  userName: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(UserTopNavIcon);
