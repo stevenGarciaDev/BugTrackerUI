ed5c04e282d838dad599a52b58d654fffe312719
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-21 21:15:03 -0800

    Update login component

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import {
   Button,
 } from '../create-project/create-project.styles';
@@ -11,24 +13,55 @@ import {
   Input,
   ButtonContainer,
 } from './login.styles';
+import { login } from '../../store/user/user.actions';
 
-const Login = () => (
-  <Container>
-    <Form>
-      <Title>Welcome back</Title>
-      <InputControl>
-        <Label>Email</Label>
-        <Input type="text" />
-      </InputControl>
-      <InputControl>
-        <Label>Password</Label>
-        <Input type="password" />
-      </InputControl>
-      <ButtonContainer>
-        <Button>Login</Button>
-      </ButtonContainer>
-    </Form>
-  </Container>
-);
+const Login = ({ performLogin }) => {
+  const [loginForm, setLoginForm] = useState({
+    Email: '',
+    Password: '',
+  });
 
-export default Login;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await performLogin(loginForm);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setLoginForm({
+      ...loginForm,
+      [name]: value,
+    });
+  };
+
+  const { Email, Password } = loginForm;
+  return (
+    <Container>
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <Title>Welcome back</Title>
+        <InputControl>
+          <Label>Email</Label>
+          <Input type="text" name="Email" value={Email} onChange={(e) => handleChange(e)} />
+        </InputControl>
+        <InputControl>
+          <Label>Password</Label>
+          <Input type="password" name="Password" value={Password} onChange={(e) => handleChange(e)} />
+        </InputControl>
+        <ButtonContainer>
+          <Button>Login</Button>
+        </ButtonContainer>
+      </Form>
+    </Container>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  performLogin: (loginForm) => dispatch(login(loginForm)),
+});
+
+Login.propTypes = {
+  performLogin: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
