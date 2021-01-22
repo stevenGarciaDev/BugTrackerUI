1d787dfd62dba6c384802fce17d827bd6372d17f
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-21 21:30:26 -0800

    Update store to use redux thunk

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [thunk];
 
 const store = createStore(
   rootReducer,
