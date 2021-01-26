56df8bdff303a75487d958d87d230c745837916b
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-26 14:45:13 -0800

    Update view project table to connect to redux store

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import ProjectsTable from '../../components/projects-table';
 import PageHeadline from '../../styles/page-headline.style';
+import { selectUserProjects } from '../../store/project/project.selector';
+import { getProjects } from '../../store/project/project.actions';
+import { selectUserToken, selectCurrentUserId } from '../../store/user/user.selector';
 
-const Projects = () => (
-  <div>
-    <PageHeadline>View Projects</PageHeadline>
-    <ProjectsTable />
-  </div>
-);
+const Projects = ({
+  userProjects, initializeProjects, userId, jwt,
+}) => {
+  useEffect(() => {
+    initializeProjects(userId, jwt);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-export default Projects;
+  return (
+    <div>
+      <PageHeadline>View Projects</PageHeadline>
+      <ProjectsTable projects={userProjects} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  userProjects: selectUserProjects(state),
+  userId: selectCurrentUserId(state),
+  jwt: selectUserToken(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  initializeProjects: (userId, jwt) => dispatch(getProjects(userId, jwt)),
+});
+
+Projects.propTypes = {
+  initializeProjects: PropTypes.func.isRequired,
+  userProjects: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+  })).isRequired,
+  userId: PropTypes.number.isRequired,
+  jwt: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Projects);
