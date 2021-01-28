625425c3c306417cf786e75b706c86cda80540e7
Steven Garcia <stevgarcia2015@gmail.com>
2021-01-27 20:46:01 -0800

    Update helper method to trimp whitespace for validating form fields

--- src/helpers/areAllFieldsFilledOut.js
+++ src/helpers/areAllFieldsFilledOut.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-restricted-syntax */
 export default function (formFields) {
   for (const field in formFields) {
-    if (formFields[field] === '') {
+    if (formFields[field].trim() === '') {
       return false;
     }
   }
