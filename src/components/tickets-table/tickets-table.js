5802cbcc3212bfa2ca85b1a429aa8a7afc329cb9
Steven Garcia <stevgarcia2015@gmail.com>
2021-03-15 10:25:21 -0700

    Update tickets table to have mobile first design

--- src/components/tickets-table/tickets-table.js
+++ src/components/tickets-table/tickets-table.js
@@ -3,23 +3,24 @@ import PropTypes from 'prop-types';
 import {
   Container, Table, TableHeadData, TableRow, TableData, Column, Button,
 } from './tickets-table.styles';
+import './tickets-table.css';
 
 const TicketsTable = ({ tickets = [] }) => (
   <Container>
     <Table>
       <colgroup>
         <Column width={350} />
-        <Column width={150} />
-        <Column width={150} />
-        <Column width={150} />
-        <Column width={150} />
+        <Column className="desktop-display" width={150} />
+        <Column className="desktop-display" width={150} />
+        <Column className="desktop-display" width={150} />
+        <Column className="desktop-display" width={150} />
       </colgroup>
       <thead>
         <TableRow>
           <TableHeadData>Title</TableHeadData>
-          <TableHeadData>Priority</TableHeadData>
-          <TableHeadData>Assigned To</TableHeadData>
-          <TableHeadData>Status</TableHeadData>
+          <TableHeadData className="desktop-display">Priority</TableHeadData>
+          <TableHeadData className="desktop-display">Assigned To</TableHeadData>
+          <TableHeadData className="desktop-display">Status</TableHeadData>
           <TableHeadData>More</TableHeadData>
         </TableRow>
       </thead>
@@ -28,10 +29,10 @@ const TicketsTable = ({ tickets = [] }) => (
         && tickets.map((t) => (
           <TableRow>
             <TableData>{t.title}</TableData>
-            <TableData>{t.priority}</TableData>
-            <TableData>{t.userName}</TableData>
-            <TableData>{t.status}</TableData>
-            <TableData><Button to={`/project/${t.projectId}/ticket/${t.id}`}>View</Button></TableData>
+            <TableData className="desktop-display">{t.priority}</TableData>
+            <TableData className="desktop-display">{t.userName}</TableData>
+            <TableData className="desktop-display">{t.status}</TableData>
+            <TableData className="view-button-container"><Button to={`/project/${t.projectId}/ticket/${t.id}`}>View</Button></TableData>
           </TableRow>
         ))}
       </tbody>
