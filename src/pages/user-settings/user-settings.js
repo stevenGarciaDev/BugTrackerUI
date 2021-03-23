2b08bd02f7e28d2efdddc44b54327140cb83a48f
Steven Garcia <stevgarcia2015@gmail.com>
2021-03-23 15:08:42 -0700

    Add styles for user settings page

--- src/pages/user-settings/user-settings.js
+++ src/pages/user-settings/user-settings.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {
+  Container, Title, UserItem, UserInfo, UserInfoTitle,
+} from './user-settings.styles';
+import { selectUser } from '../../store/user/user.selector';
 
-const UserSettings = () => (
-  <div>
-    UserSettings
-  </div>
-);
+const UserSettings = ({ user }) => {
+  const {
+    userName, email, jobTitle,
+  } = user;
+  return (
+    <Container>
+      <Title>User Information</Title>
+      <hr />
+      <UserItem>
+        <UserInfoTitle>Username</UserInfoTitle>
+        <UserInfo>{userName}</UserInfo>
+      </UserItem>
+      <UserItem>
+        <UserInfoTitle>Email</UserInfoTitle>
+        <UserInfo>{email}</UserInfo>
+      </UserItem>
+      <UserItem>
+        <UserInfoTitle>Job Title</UserInfoTitle>
+        <UserInfo>{jobTitle}</UserInfo>
+      </UserItem>
+    </Container>
+  );
+};
 
-export default UserSettings;
+const mapStateToProps = (state) => ({
+  user: selectUser(state),
+});
+
+UserSettings.propTypes = {
+  user: PropTypes.shape({
+    jobTitle: PropTypes.string.isRequired,
+    userName: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(UserSettings);
